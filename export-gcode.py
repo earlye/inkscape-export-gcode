75322d71216612f3d5917e25f31bd36b4942f4cf
Early Ehlinger <earlye@gmail.com>
2020-09-20 09:32:17 -0500

    Latest

--- export-gcode.py
+++ export-gcode.py
@@ -23,22 +23,43 @@ def getDescription(element):
     return ''
 
 class GcodeGlobals:
-    def __init__(self, fulldepth):
+    def __init__(self, fulldepth, scaleX = 1.0/25.4, scaleY = -1.0/25.4, translateX = 1, translateY = 7):
         self.fulldepth = fulldepth
         self.safeHeight = 0.25
+        self.scaleX = scaleX
+        self.scaleY = scaleY
+        self.translateX = translateX
+        self.translateY = translateY
+
+    def echo(self, stream):
+        gcode(stream, '(Globals:)\n')
+        gcode(stream,f'( .fulldepth: {self.fulldepth})\n')
+        gcode(stream,f'( .safeHeight: {self.safeHeight})\n')
+        gcode(stream,f'( .scaleX: {self.scaleX})\n')
+        gcode(stream,f'( .scaleY: {self.scaleY})\n')
+        gcode(stream,f'( .translateX: {self.translateX})\n')
+        gcode(stream,f'( .translateY: {self.translateY})\n')
+
     pass
 
+def eval(expression, default, context):
+    try:
+        return float(expression)
+    except Exception:
+        return default
+
 class GcodeSettings:
-    def __init__(self, depth, tool):
-        self.depth = depth
+    def __init__(self, depth, tool, increment = 0.1, context):
+        self.depth = eval(depth, 0.25, context)
         self.tool = tool
+        self.increment = increment
 
 def getGcodeSettings(element, gcodeGlobals):
     for child in element.iterchildren():
         if getTagName(child) == 'gcode:settings':
-            result = GcodeSettings(child.get("depth"), child.get("tool"))
+            result = GcodeSettings(child.get("depth"), child.get("tool"), child.get("increment", 0.1))
             return result
-    return GcodeSettings(0.25, '')
+    return GcodeSettings(0.25, '', 0.1)
 
 def getTagName(element):
     return os.path.basename(element.xml_path)
@@ -53,11 +74,66 @@ def gcode(stream, code):
 def gcodeSafeHeight(stream, gcodeGlobals):
     gcode(stream, f'G00 Z{gcodeGlobals.safeHeight} (raise cutter to safe height)\n')
 
+def gcodeCoordinates(X = None, Y = None, Z = None):
+    result = ''
+    if X != None:
+        result += f' X{X}'
+    if Y != None:
+        result += f' Y{Y}'
+    if Z != None:
+        result += f' Z{Z}'
+    return result.strip()
+
 def gcodeRapid(stream, X, Y, Z, comment):
-    gcode(stream, f'G00 X{X} Y{Y} ({comment})\n')
+    gcode(stream, f'G00 {gcodeCoordinates(X,Y,Z)} ({comment})\n')
 
 def gcodeLinear(stream, X, Y, Z, comment):
-    gcode(stream, f'G01 X{X} Y{Y} ({comment})\n')
+    gcode(stream, f'G01 {gcodeCoordinates(X,Y,Z)} ({comment})\n')
+
+
+def cutPathAtDepth(stream, gcodeGlobals, depth, needSafeHeight, path):
+    p0 = None
+    pInitial = None
+    gcode(stream, f'    (carving path at depth:{depth}')
+    sX = gcodeGlobals.scaleX
+    sY = gcodeGlobals.scaleY
+    tX = gcodeGlobals.translateX
+    tY = gcodeGlobals.translateY
+    for command in path:
+        letter = command.letter
+        gcode(stream, f'    (command:{command} letter:{letter} type:{type(command).__name__})\n')
+        if letter == 'M':
+            p0 = (command.args[0] * sX + tX, command.args[1] * sY + tY)
+            pInitial = p0
+            if needSafeHeight:
+                gcodeSafeHeight(stream, gcodeGlobals)
+            gcodeRapid(stream, p0[0], p0[1], None, f'rapid to start of curve')
+            gcodeLinear(stream, None, None, depth, f'plunge to depth')
+            needSafeHeight = True
+        if letter == 'C':
+            args = command.args
+            # gcode(stream, f'      (args: {args})\n')
+            p1 = (args[0] * sX + tX, args[1] * sY + tY)
+            p2 = (args[2] * sX + tX, args[3] * sY + tY)
+            p3 = (args[4] * sX + tX, args[5] * sY + tY)
+            # gcode(stream, f'      (p0: {p0} p1:{p1} p2:{p2} p3:{p3})\n')
+            for largeT in range(0,100,10):
+                t = largeT / 100.0
+                bez = [p0,p1,p2,p3]
+                pt = bezier.bezierpointatt(bez, t)
+                gcodeLinear(stream, pt[0], pt[1], None, f't:{t}')
+            gcodeLinear(stream, p3[0], p3[1], None, f't:1.0')
+            p0 = p3
+            needSafeHeight = True
+        if letter == 'L':
+            p0 = (command.args[0] * sX + tX, command.args[1] * sY + tY)
+            gcodeLinear(stream, p0[0], p0[1], None, f'line')
+            needSafeHeight = True
+        if letter == 'Z':
+            p0 = pInitial
+            gcodeLinear(stream, p0[0], p0[1], None, f'zone close')
+            needSafeHeight = False
+    return needSafeHeight
 
 def gcodePath(stream, gcodeGlobals, gcodeSettings, path):
     gcode(stream,f'  (gcodeSettings.depth: {gcodeSettings.depth})\n')
@@ -65,35 +141,14 @@ def gcodePath(stream, gcodeGlobals, gcodeSettings, path):
     gcode(stream,f'  (path commands:)\n')
     csp = CubicSuperPath(path).to_path()
     gcode(stream,f'    (cubicsuperpath parsed:{csp} type:{type(csp).__name__})\n')
-    p0 = None
-    pInitial = None
-    for command in csp:
-        letter = command.letter
-        gcode(stream, f'    (command:{command} letter:{letter} type:{type(command).__name__})\n')
-        if letter == 'M':
-            p0 = (command.args[0], -command.args[1])
-            pInitial = p0
-            gcodeSafeHeight(stream, gcodeGlobals)
-            gcodeRapid(stream, p0[0], p0[1], None, f'rapid to start of curve')
-        if letter == 'C':
-            args = command.args
-            # gcode(stream, f'      (args: {args})\n')
-            p1 = (args[0], -args[1])
-            p2 = (args[2], -args[3])
-            p3 = (args[4], -args[5])
-            # gcode(stream, f'      (p0: {p0} p1:{p1} p2:{p2} p3:{p3})\n')
-            for t in range(0,1000,100):
-                bez = [p0,p1,p2,p3]
-                pt = bezier.bezierpointatt(bez, t / 1000.0)
-                gcodeLinear(stream, pt[0], pt[1], None, f't:{t/100.0}')
-            gcodeLinear(stream, p3[0], p3[1], None, f't:1.0')
-            p0 = p3
-        if letter == 'L':
-            p0 = (command.args[0], -command.args[1])
-            gcodeLinear(stream, p0[0], p0[1], None, f'line')
-        if letter == 'Z':
-            p0 = pInitial
-            gcodeLinear(stream, p0[0], p0[1], None, f'zone close')
+
+    depth = 0
+    needSafeHeight = True
+    while depth < gcodeSettings.depth:
+        needSafeHeight = cutPathAtDepth(stream, gcodeGlobals, -depth, needSafeHeight, csp)
+        depth += gcodeSettings.increment
+    if depth != gcodeSettings.increment:
+        cutPathAtDepth(stream, gcodeGlobals, -gcodeSettings.depth, needSafeHeight, csp)
 
 def exportEllipse(ellipse, gcodeGlobals, stream):
     gcodeSettings = getGcodeSettings(ellipse, stream)
@@ -129,8 +184,7 @@ class ExportCncGcode(inkex.OutputExtension):
         # TODO: find global gcode globals in the svg XML
         gcodeGlobals = GcodeGlobals(0.80)
         gcode(stream, '\n\n')
-        gcode(stream, '(Globals:)\n')
-        gcode(stream,f'( .fulldepth: {gcodeGlobals.fulldepth})\n')
+        gcodeGlobals.echo(stream)
         gcode(stream, 'G90 (absolute distance mode)\n')
 
         for elem in self.svg.selection.paint_order().values():
