d8a1e79e37c93fff84c2c6c02d1c47db6cda1603
Early Ehlinger <earlye@gmail.com>
2020-12-12 16:18:23 -0600

    Layers can have transforms too.

--- exportLayer.py
+++ exportLayer.py
@@ -5,6 +5,10 @@ def exportLayer(stream, element, transform, methods):
     if gcodeStyle.display == 'none':
         stream.comment("Style.display:none. Skipping layer")
         return
+
+    effectiveTransform = transform.__mul__(element.composed_transform())
+    stream.comment(f"layer: originalTransform: {transform} transform: {element.composed_transform()} effectiveTransform:{effectiveTransform}")
+
     for child in reversed(list(element.iterchildren())):
-        methods['visitElement'](stream.indent(), child, transform)
+        methods['visitElement'](stream.indent(), child, effectiveTransform)
     stream.safe_height(gcodeStyle)
